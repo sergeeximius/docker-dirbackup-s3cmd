db1f2a61bb9d097e972dfda3eae5a740ef416cd0
Sergey Sedov <ssedov@yandex.ru>
2024-03-21 15:35:56 +0400

    feat: Add cifs and rotation blank

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
 APP_NAME="ssedov/dirbackup-s3cmd"
-TAG="2.1"
+TAG ?= latest
 
 # HELP
 # This will output the help for each task
-# thanks to https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
 .PHONY: help
 help: ## This help.
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 .DEFAULT_GOAL := help
 
-build: ## Build the image
-	docker buildx create --name multi-arch --driver docker-container --use
-	docker buildx inspect --bootstrap
-	docker buildx build --platform linux/amd64,linux/arm64 -t $(APP_NAME):$(TAG) --pull --push docker
-	docker buildx rm multi-arch
+build: ## Build the container image with tag (e.g. make build TAG=latest)
+	@docker buildx create --name multi-arch --driver docker-container --use
+	@docker buildx inspect --bootstrap
+	@docker buildx build --platform linux/amd64,linux/arm64 -t $(APP_NAME):$(TAG) --pull --push docker
+	@docker buildx rm multi-arch
+
+build-latest: TAG=latest
+build-latest: build ## Build the image with the 'latest' tag
+
+build-dev: TAG=dev
+build-dev: build ## Build the image with the 'dev' tag
 
 shell: ## Creates a shell inside the container for debug purposes
-	docker run -it $(APP_NAME):$(TAG) bash
+	@docker run -it $(APP_NAME):$(TAG) bash
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,56 +4,150 @@ if [[ "$@" == "bash" ]]; then
     exec $@
 fi
 
-test -z "$BACKUP_DIR" && BACKUP_DIR='' || BACKUP_DIR=/${BACKUP_DIR}
-test -z "$BACKUP_NAME" && BACKUP_NAME='data' || BACKUP_NAME=${BACKUP_NAME}
-if [[ ! -z "$BACKUP_EXCLUDE" ]]; then
-  if [[ $BACKUP_EXCLUDE =~ ^[a-zA-Z0-9,_.\*-]+$ ]]; then
-    IFS=',' read -r -a dirs <<< "$BACKUP_EXCLUDE"
-    EXCLUDE_PARAMS=()
-    for dir in "${dirs[@]}"; do
-      EXCLUDE_PARAMS+=("--exclude=$dir")
-    done
-    echo "Use tar options: "${EXCLUDE_PARAMS[@]}""
-  else
-    echo "BACKUP_EXCLUDE contains invalid characters, example 'ansible,.terraform,*.sql' without spaces"
-    exit 1
-  fi
+if [[ ! -z "$BACKUP_SRC" ]]; then
+    if [[ $BACKUP_SRC = cifs ]]; then
+        test -z "$CIFS_HOST" && echo "CIFS_HOST is not defined" && exit 1
+        test -z "$CIFS_SHARE" && echo "CIFS_SHARE is not defined" && exit 1
+        test -z "$CIFS_USER" && echo "CIFS_USER is not defined" && exit 1
+        test -z "$CIFS_PASSWORD" && echo "CIFS_PASSWORD is not defined" && exit 1
+        echo "username=$CIFS_USER" >>~/.smb-credentials
+        echo "password=$CIFS_PASSWORD" >>~/.smb-credentials
+        test -z "$CIFS_DOMAIN" || echo "domain=$CIFS_DOMAIN" >>~/.smb-credentials
+        mkdir -p /data
+        echo "Mount CIFS share //${CIFS_HOST}/${CIFS_SHARE}"
+        mount.cifs -o credentials=~/.smb-credentials,iocharset=utf8 //${CIFS_HOST}/${CIFS_SHARE} /data
+    else
+        echo "BACKUP_SRC can be set to 'local' or 'cifs' to determine the backup source type"
+        exit 1
+    fi
 else
-  EXCLUDE_PARAMS=''
+    backup_src='local'
+fi
+test -z "$BACKUP_DIR" && backup_dir='' || backup_dir=/${BACKUP_DIR}
+test -z "$BACKUP_NAME" && backup_name='data' || backup_name=${BACKUP_NAME}
+if [[ ! -z "$BACKUP_EXCLUDE" ]]; then
+    if [[ $BACKUP_EXCLUDE =~ ^[a-zA-Z0-9,_.\*-]+$ ]]; then
+        IFS=',' read -r -a dirs <<<"$BACKUP_EXCLUDE"
+        exclude_params=()
+        for dir in "${dirs[@]}"; do
+            exclude_params+=("--exclude=$dir")
+        done
+        exclude_params+=("--exclude=*.DS_Store")
+        echo "Use tar options: "${exclude_params[@]}""
+    else
+        echo "BACKUP_EXCLUDE contains invalid characters, example 'ansible,.terraform,*.sql' without spaces"
+        exit 1
+    fi
+else
+    exclude_params=("--exclude=*.DS_Store")
 fi
 test -z "$S3_BACKET" && echo "S3_BACKET is not defined" && exit 1
 test -z "$S3_ACCESS_KEY" && echo "S3_ACCESS_KEY is not defined" && exit 1
 test -z "$S3_SECRET_KEY" && echo "S3_SECRET_KEY is not defined" && exit 1
-test -z "$S3_PATH" && S3_PATH=''
-test -z "$S3_NAME_PREFIX" && S3_NAME_PREFIX='' || S3_NAME_PREFIX=${S3_NAME_PREFIX}_
-test -z "$S3_PROVIDER" && S3_PROVIDER='yandex'
+test -z "$S3_PATH" && s3_path='' || s3_path=/${S3_PATH}
+test -z "$S3_NAME_PREFIX" && s3_name_prefix='' || s3_name_prefix=${S3_NAME_PREFIX}_
+test -z "$S3_PROVIDER" && s3_provider='yandex' || s3_provider=${S3_PROVIDER}
 
-POSTFIX=$(date +%Y-%m-%d).tar
+postfix=$(date +%Y-%m-%d).tar
 
-if [ "$S3_PROVIDER" = "selectel" ]; then
-  mv /root/.s3cfg_selectel ~/.s3cfg
-else
-  mv /root/.s3cfg_yandex ~/.s3cfg
-fi
+mv /root/.s3cfg_$([ "$s3_provider" = "selectel" ] && echo "selectel" || echo "yandex") ~/.s3cfg
+echo "access_key = $S3_ACCESS_KEY" >>~/.s3cfg
+echo "secret_key = $S3_SECRET_KEY" >>~/.s3cfg
 
-echo "access_key = $S3_ACCESS_KEY" >> ~/.s3cfg
-echo "secret_key = $S3_SECRET_KEY" >> ~/.s3cfg
+backup_bin=$([ "$BACKUP_FORMAT" = "xz" ] && echo "xz" || echo "gzip")
+backup_ext=$([ "$BACKUP_FORMAT" = "xz" ] && echo ".xz" || echo ".gz")
 
-tar "${EXCLUDE_PARAMS[@]}" -cf /${BACKUP_NAME}_${POSTFIX} data${BACKUP_DIR}
+# Функция для проверки, содержится ли дата в массиве
+date_in_array() {
+    local date_to_check=$1
+    shift
+    local dates_array=("$@")
+    for date in "${dates_array[@]}"; do
+        if [[ "$date" == "$date_to_check" ]]; then
+            return 0 # Дата найдена
+        fi
+    done
+    return 1 # Дата не найдена
+}
 
-if [ "$BACKUP_FORMAT" = "xz" ]; then
-  BACKUP_BIN="xz"
-  BACKUP_EXT=".xz"
-else
-  BACKUP_BIN="gzip"
-  BACKUP_EXT=".gz"
-fi
+# Функция для вывода файлов которые должны остаться после ротации
+# Последние 6 файлов и 1 последний файл за каждую неделю
+function get_files_to_pass() {
+    local all_files=("$@")
+    local -a to_pass=()
 
-$BACKUP_BIN /${BACKUP_NAME}_${POSTFIX}
-s3cmd --storage-class COLD put /${BACKUP_NAME}_${POSTFIX}${BACKUP_EXT} s3://${S3_BACKET}/${S3_PATH}/${S3_NAME_PREFIX}${BACKUP_NAME}_${POSTFIX}${BACKUP_EXT}
+    local -a last_weekly=()
+    # local -a last_mondays=()
+    # local -a last_sundays=()
+    declare -A latest_file_per_week
+
+    for ((i = 1; i <= 7; i++)); do
+        local last_day=$(date -d "today - $((i - 1)) days" +%Y-%m-%d)
+        last_weekly+=("$last_day")
+    done
+
+    # for ((i = 1; i <= 15; i++)); do
+    #     local last_monday=$(date -d "last monday - $((i - 1)) weeks" +%Y-%m-%d)
+    #     last_mondays+=("$last_monday")
+    #     local last_sunday=$(date -d "last sunday - $((i - 1)) weeks" +%Y-%m-%d)
+    #     last_sundays+=("$last_sunday")
+    # done
+
+    for file in "${all_files[@]}"; do
+        file_date_str=$(basename "$file" | egrep -o "\d{4}-\d{2}-\d{2}")
+
+        if date_in_array "$file_date_str" "${last_weekly[@]}"; then
+            to_pass+=("${file}_pass_days")
+            continue
+        fi
+
+        week_of_year=$(date -d "$file_date_str" +%Y-%W)
+        latest_file_date_str=$(basename "${latest_file_per_week[$week_of_year]}" | egrep -o "\d{4}-\d{2}-\d{2}")
+        if [ -z "${latest_file_per_week[$week_of_year]}" ] || [ "$(date -d "$latest_file_date_str" +%s)" -le "$(date -d "$file_date_str" +%s)" ]; then
+            latest_file_per_week[$week_of_year]=$file
+        fi
+    done
+
+    for week in "${!latest_file_per_week[@]}"; do
+        to_pass+=("${latest_file_per_week[$week]}_pass_weekly")
+    done
+
+    echo "${to_pass[@]}"
+}
+
+tar "${exclude_params[@]}" -cf /${backup_name}_${postfix} data${backup_dir}
+
+$backup_bin /${backup_name}_${postfix}
+s3cmd --storage-class COLD put /${backup_name}_${postfix}${backup_ext} s3://${S3_BACKET}${s3_path}/${s3_name_prefix}${backup_name}_${postfix}${backup_ext}
 
 if [ $? -ne 0 ]; then
-  exit 1
+    echo "Command s3cmd return code $?. Exit"
+    exit 1
+fi
+
+if [[ ! -z "$ROTATION" ]]; then
+    # Получение списка файлов в bucket и сортировка (новейшие файлы вверху).
+    all_files=($(s3cmd ls s3://${S3_BACKET}${s3_path}/ | awk '{print $4}' | grep -- "${s3_name_prefix}${backup_name}" | sort -r))
+    # Определение файлов для пропуска.
+    echo $(get_files_to_pass "${all_files[@]}") >/tmp/files_to_pass.txt
+
+    # Вывод информации или ротация
+    for file in "${all_files[@]}"; do
+        if [[ ! -z "$ROTATION_DRY_RUN" ]]; then
+            if grep -q "$file" /tmp/files_to_pass.txt; then
+                echo "Will be passed: $file"
+            else
+                echo "Will be deleted: $file"
+            fi
+        else
+            if grep -q "$file" /tmp/files_to_pass.txt; then
+                echo "Passing: $file"
+            else
+                echo "Deleting: $file"
+                #s3cmd del "$file"
+            fi
+        fi
+    done
 fi
 
 exec "$@"
